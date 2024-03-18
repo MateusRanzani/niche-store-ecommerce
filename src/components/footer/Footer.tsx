36300a5e6e0ac912efaf2f6b0e684cc948855a46
mateusRanzani <mateusranzani@hotmail.com>
2024-03-18 08:09:01 -0300

    Update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,49 @@
 import { HomeIcon } from "@/components/icons/HomeIcon";
 import Link from "next/link";
+import {
+  TiSocialFacebookCircular,
+  TiSocialInstagramCircular,
+} from "react-icons/ti";
 
 export const Footer = () => {
   return (
     <footer className="w-full border-t border-gray-200 border-gray-200 bg-white dark:border-gray-800 dark:bg-gray-950">
       <div className="container grid gap-12 px-4 py-12 md:grid-cols-2 md:px-6 lg:gap-16">
         <div className="flex flex-col gap-4">
-          <Link className="flex items-center space-x-2" href="#">
+          <Link
+            className="flex items-center justify-center lg:justify-normal space-x-2"
+            href="#"
+          >
             <HomeIcon className="w-5 h-5" />
             <span className="font-semibold">Home</span>
           </Link>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
+
+          <div className="flex gap-2 flex justify-center lg:justify-normal">
+            <Link className="flex items-center hover:scale-105" href="#">
+              <TiSocialFacebookCircular className="text-2xl" />
+            </Link>
+
+            <Link className="flex items-center hover:scale-105" href="#">
+              <TiSocialInstagramCircular className="text-2xl" />
+            </Link>
+          </div>
+
+          <p className="text-sm text-gray-500 dark:text-gray-400 cursor-pointer  flex justify-center lg:justify-normal">
+            customer@nichestore.com.br
+          </p>
+
+          <p className="text-sm text-gray-500 dark:text-gray-400  flex justify-center lg:justify-normal">
             © 2023 Niche Store Ecommerce. All rights reserved.
           </p>
         </div>
-        <div className="grid gap-4">
-          <Link className="font-semibold" href="#">
+        <div className="grid gap-4  flex justify-center lg:justify-normal">
+          <Link className="font-semibold text-center lg:text-start" href="#">
             Contact Us
           </Link>
-          <Link className="font-semibold" href="#">
+          <Link className="font-semibold text-center lg:text-start" href="#">
             Privacy Policy
           </Link>
-          <Link className="font-semibold" href="#">
+          <Link className="font-semibold text-center lg:text-start" href="#">
             Terms & Conditions
           </Link>
         </div>
