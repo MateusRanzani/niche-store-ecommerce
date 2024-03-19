f1ea808d431013f7c3d299051c976f04c359924c
mateusRanzani <mateusranzani@hotmail.com>
2024-03-18 21:44:46 -0300

    Add rating in card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import ReactStars from "react-stars";
 import Link from "next/link";
 
 interface ItemInterface {
@@ -17,7 +18,7 @@ interface ItemInterface {
 
 const Card = (item: ItemInterface) => {
   return (
-    <div className="w-full h-auto	 rounded border border-[#ECECEC] ">
+    <div className="w-full h-auto p-4 rounded border border-[#ECECEC] ">
       <div className="relative w-full h-40 my-4">
         <Image
           src={item.image}
@@ -26,31 +27,41 @@ const Card = (item: ItemInterface) => {
           fill
         />
       </div>
-      <div className="flex justify-center  md:min-h-20">
-        <p className="w-11/12 font-bold text-1xl my-4 line-clamp-2 cursor-pointer">
+
+      <div className="flex flex-col items-center md:items-start">
+        <p className="w-12/12 font-bold text-1xl my-4 line-clamp-2 cursor-pointer md:min-h-[3rem]">
           {item.title}
         </p>
-      </div>
-      <div className="flex justify-center">
-        <p className="w-11/12 font-bold text-2xl ">
+
+        <ReactStars
+          count={5}
+          value={item.rating.rate}
+          size={18}
+          edit={false}
+          color1={"#ECECEC"}
+          color2={"black"}
+        />
+
+        <p className="w-12/12 font-bold text-2xl ">
           {item.price.toLocaleString("en-US", {
             style: "currency",
             currency: "USD",
           })}
         </p>
-      </div>
-      <div className="w-full  my-4">
-        <Link
-          className="w-full flex justify-center font-semibold"
-          href={`/product/${item.id}`}
-        >
-          <Button
-            className="w-11/12 bg-blue-500 hover:bg-blue-600 text-white hover:text-white"
-            variant="outline"
+
+        <div className="w-full my-4">
+          <Link
+            className="w-full flex justify-center font-semibold"
+            href={`/product/${item.id}`}
           >
-            Buy Now
-          </Button>
-        </Link>
+            <Button
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white hover:text-white"
+              variant="outline"
+            >
+              Buy Now
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
